7d16d128eeda87bbe0d9e51c96f071cb8af6ff58
Mwangi-dan <d.ndungu@alustudent.com>
2024-07-28 11:27:39 +0300

    app update

--- summative/FlutterApp/cell_prediction/lib/main.dart
+++ summative/FlutterApp/cell_prediction/lib/main.dart
@@ -12,8 +12,9 @@ class _PredictionFormPageState extends State<PredictionFormPage> {
   final Map<String, dynamic> _formData = {
     "weight": null,
     "resolution": null,
-    "pixels per inch": '',
-    "cpu_core": '',
+    "pixels_per_inch": null,
+    "cpu_core": null,
+    "cpu_freq": '',
     "internal_mem": '',
     "ram": null,
     "RearCam": null,
@@ -24,39 +25,56 @@ class _PredictionFormPageState extends State<PredictionFormPage> {
   String _predictedPrice = '';
   bool _isLoading = false;
   double _formOpacity = 1.0;
+  List<Map<String, dynamic>> _previousPredictions = [];
 
   Future<void> _submitForm() async {
-    setState(() {
-      _isLoading = true;
-      _formOpacity = 0.5;
-    });
+    try {
+      setState(() {
+        _isLoading = true;
+        _formOpacity = 0.5;
+      });
 
-    final url =
-        Uri.parse('https://linear-regression-model-w1my.onrender.com/predict');
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode({
-        "weight": double.parse(_formData["weight"]),
-        "resolution": double.parse(_formData["resolution"]),
-        "ppi": int.parse(_formData["pixels per inch"]),
-        "cpu_core": int.parse(_formData["cpu_core"]),
-        "cpu_freq": double.parse(_formData["cpu_freq"]),
-        "internal_mem": double.parse(_formData["internal_mem"]),
-        "ram": double.parse(_formData["ram"]),
-        "RearCam": double.parse(_formData["RearCam"]),
-        "Front_Cam": double.parse(_formData["Front_Cam"]),
-        "battery": int.parse(_formData["battery"]),
-        "thickness": double.parse(_formData["thickness"]),
-      }),
-    );
+      final url = Uri.parse(
+          'https://linear-regression-model-w1my.onrender.com/predict');
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          "weight": double.parse(_formData["weight"].toString()),
+          "resoloution": double.parse(_formData["resolution"].toString()),
+          "ppi": int.parse(_formData["pixels_per_inch"].toString()),
+          "cpu_core": int.parse(_formData["cpu_core"].toString()),
+          "cpu_freq": double.parse(_formData["cpu_freq"].toString()),
+          "internal_mem": double.parse(_formData["internal_mem"].toString()),
+          "ram": double.parse(_formData["ram"].toString()),
+          "RearCam": double.parse(_formData["RearCam"].toString()),
+          "Front_Cam": double.parse(_formData["Front_Cam"].toString()),
+          "battery": int.parse(_formData["battery"].toString()),
+          "thickness": double.parse(_formData["thickness"].toString()),
+        }),
+      );
 
-    final responseData = json.decode(response.body);
-    setState(() {
-      _predictedPrice = responseData['predicted_price'].toString();
-      _isLoading = false;
-      _formOpacity = 1.0;
-    });
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        setState(() {
+          _predictedPrice =
+              double.parse(responseData['predicted_price'].toString())
+                  .toStringAsFixed(3);
+          _previousPredictions
+              .add({..._formData, "predicted_price": _predictedPrice});
+          _isLoading = false;
+          _formOpacity = 1.0;
+        });
+      } else {
+        throw Exception('Failed to load data from API');
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _formOpacity = 1.0;
+        _predictedPrice = "Error occurred: $e";
+      });
+    }
   }
 
   @override
@@ -69,89 +87,134 @@ class _PredictionFormPageState extends State<PredictionFormPage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                AnimatedOpacity(
-                  opacity: _formOpacity,
-                  duration: Duration(milliseconds: 500),
-                  child: Column(
-                    children: [
-                      _buildDropdownField("Weight", "weight",
-                          [110.0, 125.0, 135.0, 150.0, 170.0, 202.0]),
-                      _buildDropdownField("Resolution", "resolution",
-                          [4.0, 4.7, 5.2, 5.5, 6.0]),
-                      _buildTextField("Pixels per inch", "pixels per inch"),
-                      _buildTextField("CPU Core", "cpu_core"),
-                      _buildTextField("Internal Memory", "internal_mem"),
-                      _buildDropdownField(
-                          "RAM", "ram", [0.512, 1.0, 1.5, 3.0, 4.0, 6.0]),
-                      _buildDropdownField("Rear Camera", "RearCam",
-                          [3.15, 12.0, 13.0, 20.0, 21.5]),
-                      _buildDropdownField("Front Camera", "Front_Cam",
-                          [0.0, 5.0, 8.0, 16.0, 20.0]),
-                      _buildTextField("Battery", "battery"),
-                      _buildTextField("Thickness", "thickness"),
-                      SizedBox(height: 20),
-                      AnimatedContainer(
-                        duration: Duration(milliseconds: 500),
-                        width: _isLoading ? 50 : double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              _formKey.currentState!.save();
-                              _submitForm();
-                            }
-                          },
-                          child: _isLoading
-                              ? SizedBox(
-                                  width: 24,
-                                  height: 24,
-                                  child: CircularProgressIndicator(
-                                    color: Colors.white,
-                                  ),
-                                )
-                              : Text('Predict'),
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: Colors.blueAccent,
-                            padding: EdgeInsets.symmetric(vertical: 16.0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0),
+          child: Column(
+            children: [
+              Text(
+                'Welcome to the Cell Phone Price Predictor!',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Fill in the specifications below to get an estimated price for your device.',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    AnimatedOpacity(
+                      opacity: _formOpacity,
+                      duration: Duration(milliseconds: 500),
+                      child: Column(
+                        children: [
+                          _buildDropdownField("Weight", "weight",
+                              [110.0, 125.0, 135.0, 150.0, 170.0, 202.0]),
+                          _buildDropdownField("Resolution", "resolution",
+                              [4.0, 4.7, 5.2, 5.5, 6.0]),
+                          _buildDropdownField("Pixels per inch",
+                              "pixels_per_inch", [233, 312, 401, 424, 534]),
+                          _buildDropdownField(
+                              "CPU Core", "cpu_core", [2, 4, 8]),
+                          _buildTextField("CPU Freq", "cpu_freq"),
+                          _buildTextField("Internal Memory", "internal_mem"),
+                          _buildDropdownField(
+                              "RAM", "ram", [0.512, 1.0, 1.5, 3.0, 4.0, 6.0]),
+                          _buildDropdownField("Rear Camera", "RearCam",
+                              [3.15, 12.0, 13.0, 20.0, 21.5]),
+                          _buildDropdownField("Front Camera", "Front_Cam",
+                              [0.0, 5.0, 8.0, 16.0, 20.0]),
+                          _buildTextField("Battery", "battery"),
+                          _buildTextField("Thickness", "thickness"),
+                          const SizedBox(height: 20),
+                          AnimatedContainer(
+                            duration: Duration(milliseconds: 500),
+                            width: _isLoading
+                                ? 50
+                                : MediaQuery.of(context).size.width * 0.9,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState!.save();
+                                  _submitForm();
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.blueAccent,
+                                padding: EdgeInsets.symmetric(vertical: 16.0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                              child: _isLoading
+                                  ? SizedBox(
+                                      width: 24,
+                                      height: 24,
+                                      child: CircularProgressIndicator(
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                  : Text('Predict'),
                             ),
                           ),
-                        ),
+                          SizedBox(height: 20),
+                          if (_predictedPrice.isNotEmpty)
+                            AnimatedOpacity(
+                              opacity: _predictedPrice.isNotEmpty ? 1.0 : 0.0,
+                              duration: Duration(milliseconds: 500),
+                              child: Text(
+                                'Predicted Price: $_predictedPrice',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                        ],
                       ),
-                      SizedBox(height: 20),
-                      if (_predictedPrice.isNotEmpty)
-                        AnimatedOpacity(
-                          opacity: _predictedPrice.isNotEmpty ? 1.0 : 0.0,
-                          duration: Duration(milliseconds: 500),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              if (_previousPredictions.isNotEmpty)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Previous Predictions:',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    ..._previousPredictions.map((prediction) {
+                      return Card(
+                        margin: EdgeInsets.symmetric(vertical: 8.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
                           child: Text(
-                            'Predicted Price: $_predictedPrice',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green,
-                            ),
+                            'Weight: ${prediction["weight"]}, Resolution: ${prediction["resolution"]}, PPI: ${prediction["pixels_per_inch"]}, CPU Core: ${prediction["cpu_core"]}, CPU Freq: ${prediction["cpu_freq"]}, Internal Mem: ${prediction["internal_mem"]}, RAM: ${prediction["ram"]}, RearCam: ${prediction["RearCam"]}, FrontCam: ${prediction["Front_Cam"]}, Battery: ${prediction["battery"]}, Thickness: ${prediction["thickness"]} => Predicted Price: \$${prediction["predicted_price"]}',
+                            style: TextStyle(fontSize: 14),
                           ),
                         ),
-                    ],
-                  ),
+                      );
+                    }).toList(),
+                  ],
                 ),
-              ],
-            ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget _buildDropdownField(String label, String key, List<double> options) {
+  Widget _buildDropdownField(String label, String key, List<dynamic> options) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: DropdownButtonFormField<double>(
+      child: DropdownButtonFormField<dynamic>(
         decoration: InputDecoration(
           labelText: label,
           border: OutlineInputBorder(),
@@ -159,8 +222,8 @@ class _PredictionFormPageState extends State<PredictionFormPage> {
           fillColor: Colors.grey[200],
         ),
         value: _formData[key],
-        items: options.map((double value) {
-          return DropdownMenuItem<double>(
+        items: options.map((dynamic value) {
+          return DropdownMenuItem<dynamic>(
             value: value,
             child: Text(value.toString()),
           );
